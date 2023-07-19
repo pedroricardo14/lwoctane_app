5805157635eeba9c7af92bc7be8a413b4c2a8558
pedroricardo14 <pedroricardo1032@gmail.com>
2023-07-19 11:48:36 -0300

    commit

--- my-project/routes/web.php
+++ my-project/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Livewire\Counter;
+use App\Http\Livewire\Index;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::get('/contador', Counter::class);
+Route::get('/site', Index::class);
